a9963680f211b2cc7bfedb8656dff7284bf45fc3
Rajabbb <burzuyevrcb@gmail.com>
2026-08-23 15:01:36 +0400

    Update main.py

--- main.py
+++ main.py
@@ -426,7 +426,7 @@ def get_carrier_quote_page(token: str):
 @app.get("/admin/generate-invite")
 def generate_invite_link(secret: str = None):
     # DİQQƏT: Bu gizli parolu kimsə bilməsə link yarada bilməz
-    ADMIN_SECRET_KEY = "arachi_super_gizli_2026_!#" 
+    ADMIN_SECRET_KEY = "arachi_super_gizli_recebvusal2026" 
     
     if secret != ADMIN_SECRET_KEY:
         raise HTTPException(status_code=403, detail="Giriş qadağandır! Gizli şifrə səhvdir.")
